afe7e8997298ae70ab68a10a81e280a76f7507d0
Pawan Kumar <Pawankumar@t-a-g.net>
2023-10-10 08:12:19 +0530

    Initialize the UI interface through html, css and javascript code

---
+++ styles.cs
@@ -0,0 +1,28 @@
+body {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+    background-color: #f4f4f4;
+}
+
+.container {
+    max-width: 800px;
+    margin: 0 auto;
+    padding: 20px;
+    background-color: #fff;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+ul {
+    list-style: none;
+    padding: 0;
+}
+
+li {
+    margin-bottom: 10px;
+}
+
+h1 {
+    font-size: 24px;
+    margin-bottom: 20px;
+}
